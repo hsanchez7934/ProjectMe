fe9102b0f9786b4bbf0dfd2727b4611bbdaf8d59
Hector Sanchez <hsanchez7934@gmail.com>
2017-10-25 19:55:29 -0600

    Initial push

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    // fetch('http://api.forismatic.com/api/1.0/')
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+}
+  render() {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;
